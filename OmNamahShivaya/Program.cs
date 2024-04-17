4ac0a1d57a3d40624709cd57b79ef828236190a5
DotcraftInnovations <DotcraftInnovations@LAPTOP-PH67IQOQ>
2024-04-17 22:30:00 +0530

    first commit

---
+++ OmNamahShivaya/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmNamahShivaya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Om Namah Shivaya..");
+            Console.ReadKey();
+        }
+    }
+}
